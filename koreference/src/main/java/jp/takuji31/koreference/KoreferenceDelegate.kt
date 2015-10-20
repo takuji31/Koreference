cd3398a2d1216ba5958d2caf1fedbad5401780c9
Takuji Nishibayashi <takuji31@gmail.com>
2015-10-20 22:10:58 +0900

    rawDefaultValue should be private

--- koreference/src/main/java/jp/takuji31/koreference/KoreferenceDelegate.kt
+++ koreference/src/main/java/jp/takuji31/koreference/KoreferenceDelegate.kt
@@ -10,7 +10,7 @@ import kotlin.properties.ReadWriteProperty
  */
 public abstract class KoreferenceDelegate<M : Any?, P : Any?>(val default: M, val name: String? = null) : ReadWriteProperty<SharedPreferences, M>, Preference<P>, ValueConverter<P, M> {
 
-    val rawDefaultValue: P by lazy(LazyThreadSafetyMode.NONE) {
+    private val rawDefaultValue: P by lazy(LazyThreadSafetyMode.NONE) {
         toPreferenceValue(default)
     }
 
