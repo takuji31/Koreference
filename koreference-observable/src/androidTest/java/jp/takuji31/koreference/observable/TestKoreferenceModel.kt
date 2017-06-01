7f3fb906e3232ea9686d296e7decf5d9fc610d15
Takuji Nishibayashi <takuji31@gmail.com>
2017-06-01 18:52:26 +0900

    reduce properties

--- koreference-observable/src/androidTest/java/jp/takuji31/koreference/observable/TestKoreferenceModel.kt
+++ koreference-observable/src/androidTest/java/jp/takuji31/koreference/observable/TestKoreferenceModel.kt
@@ -1,15 +1,10 @@
 package jp.takuji31.koreference.test
 
 import android.content.SharedPreferences
-import jp.takuji31.koreference.*
+import jp.takuji31.koreference.KoreferenceModel
+import jp.takuji31.koreference.stringPreference
 
 class TestKoreferenceModel(pref: SharedPreferences) : KoreferenceModel(pref = pref) {
     var stringValue: String by stringPreference("default value")
-    var intValue: Int by intPreference(256)
-    var longValue: Long by longPreference(256L)
-    var floatValue: Float by floatPreference(12.34f)
-    var boolValue: Boolean by booleanPreference(true)
-    var stringSetValue: Set<String> by stringSetPreference(setOf())
-
-    var noKoreferenceProperty : String = ""
+    var noKoreferenceProperty: String = ""
 }
