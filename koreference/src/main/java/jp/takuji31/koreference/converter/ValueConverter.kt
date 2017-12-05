b661fe31d0309a0c506db795902d03f74c1ae383
Takuji Nishibayashi <takuji31@gmail.com>
2017-12-05 20:53:29 +0900

    fix type param name

--- koreference/src/main/java/jp/takuji31/koreference/converter/ValueConverter.kt
+++ koreference/src/main/java/jp/takuji31/koreference/converter/ValueConverter.kt
@@ -3,9 +3,9 @@ package jp.takuji31.koreference.converter
 /**
  * Created by takuji on 2015/08/14.
  */
-interface ValueConverter<P : Any?, ModelValue : Any?> {
-    fun toPreferenceValue(value: ModelValue): P
-    fun toModelValue(value: P): ModelValue
+interface ValueConverter<P : Any?, M : Any?> {
+    fun toPreferenceValue(value: M): P
+    fun toModelValue(value: P): M
 
     class Raw<T: Any?> : ValueConverter<T, T> {
         override fun toPreferenceValue(value: T): T {
